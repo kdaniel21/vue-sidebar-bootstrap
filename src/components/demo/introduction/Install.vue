da473619d0ab57bacec3bcdb1da2491198fec5d5
Daniel Kiss <kissd621@gmail.com>
2020-11-02 17:43:21 +0100

    Added installation docs

--- src/components/demo/introduction/Install.vue
+++ src/components/demo/introduction/Install.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="install">
     <h3>Installation</h3>
+    <pre>npm install vue-sidebar-bootstrap</pre>
+    Then:
+    <div class="font-weight-bold">main.js</div>
+    <pre>
+      import VueSidebarBootstrap from 'vue-sidebar-bootstrap'
+      ...
+      Vue.use(VueSidebarBootstrap)
+    </pre>
+
+    After these steps you can use all the components (vsb-sidebar, vsb-layout)
+    application-wide.
   </div>
 </template>
 
